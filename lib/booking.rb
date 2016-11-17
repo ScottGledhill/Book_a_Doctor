38bea15ed5499e3074a9030b4b89cad654696d5e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-17 18:09:25 +0000

    refactored booking methods

--- lib/booking.rb
+++ lib/booking.rb
@@ -5,8 +5,16 @@ class Booking
   attr_reader :availability
 
   def initialize
+    access_file
+    add_file_to_database
+  end
+
+  def access_file
     file = open("availability.rb", "r")
     @availability = JSON.parse(file.read)
+  end
+
+  def add_file_to_database
     availability["availability_slots"].each do |avail|
       DoctorAvailablity.create(:time => avail["time"], :availability => true, :slot_size => avail["slot_size"], :doctor_id => avail["doctor_id"])
     end
