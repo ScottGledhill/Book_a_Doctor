865cad8ced6989e17e6c64385a7d387ecff47316
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-17 18:23:28 +0000

    test for checking availability

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -6,4 +6,8 @@ describe Booking do
   it 'loads availability file when class created' do
     expect(booking.availability).to include {"availability_slots"}
   end
+
+  it 'can check availability and return the booking time' do
+    expect(booking.check_availability('08:00:00')).to eq '08:00:00'
+  end
 end
